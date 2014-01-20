993d969893aed829fa287e95222de3fd90e0ba38
Bart van den Eijnden <bartvde@boundlessgeo.com>
2014-01-20 13:39:35 +0100

    work in progress, map content still needs to be moved to home partial, more bindings needed, grouping still missing

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,42 @@
 'use strict';
 
+var map = new ol.Map({
+    target: 'map',
+    renderer: ol.RendererHint.CANVAS,
+    layers: [new ol.layer.Tile({
+        title: "Streets",
+        group: "Base Maps",
+        exclusive: true,
+        source: new ol.source.MapQuest({layer: 'osm'})
+    }),
+    new ol.layer.Tile({
+        title: "Aerial",
+        group: "Base Maps",
+        exclusive: true,
+        visible: false,
+        source: new ol.source.MapQuest({layer: 'sat'})
+    }),
+    new ol.layer.Vector({
+        title: 'Zoning',
+        group: 'Overlays',
+        style: new ol.style.Style({symbolizers: [
+            new ol.style.Stroke({
+                color: 'black',
+                width: 4,
+                opacity: 1
+            })
+        ]}),
+        source: new ol.source.Vector({
+            parser: new ol.parser.GeoJSON(),
+            url: 'data/medford-zoning.json'
+        })
+    })],
+    view: new ol.View2D({
+        center: ol.proj.transform(
+            [-122.85676399771559, 42.3389246879193], 'EPSG:4326', 'EPSG:3857'),
+        zoom: 12
+    })
+});
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
@@ -10,7 +47,7 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/home', {templateUrl: 'partials/home.html'});
+  $routeProvider.when('/about', {templateUrl: 'partials/about.html'});
+  $routeProvider.otherwise({redirectTo: '/home'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,4 +8,18 @@ angular.module('myApp.controllers', []).
   }])
   .controller('MyCtrl2', [function() {
 
+  }])
+  .controller('LayerSwitcherCtrl', ['$scope', function($scope) {
+    $scope.layers = [];
+    $scope.updateVisiblity = function(layer) {
+      var title = layer.title;
+      map.getLayers().forEach(function(lyr) {
+        if (title === lyr.get('title')) {
+          lyr.set('visible', layer.visible);
+        }
+      });
+    };
+    map.getLayers().forEach(function(lyr) {
+      $scope.layers.push({title: lyr.get('title'), visible: lyr.get('visible')});
+    });
   }]);
